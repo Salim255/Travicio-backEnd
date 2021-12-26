d6c6b3e710f126c90318bf53118927f938bf1ba1
Salim <doshka23@gmail.com>
2021-12-26 14:32:18 +0100

    Get All Profiles & Profile By User ID

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -116,5 +116,44 @@ async(req, res) =>{
         console.error(error.message)
     }
     
-})
+});
+
+//1route Get api/profile
+//2description Gret all prfiles
+//3access Public
+
+router.get('/', async(req, res) =>{
+    try {
+        const profiles = await Profile.find().populate('user', ['name','avatar']);
+        res.json({profiles});
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+//1route Get api/profile/user/:user_id
+//2description Gret profile by  user_id
+//3access Public
+router.get('/user/:user_id', async(req, res) =>{
+    try {
+        const profile = await Profile.findOne({user: req.params.user_id}).populate('user', ['name','avatar']);
+
+        if(!profile){
+            return res.status(400).json({
+                msg: 'Profile not found'
+            });
+        }
+        res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+
+        if(error.kind == 'ObjectId'){
+            return res.status(400).json({
+                msg: 'Profile not found'
+            });
+        };
+        res.status(500).send('Server Error');
+    }
+});
 module.exports = router;
