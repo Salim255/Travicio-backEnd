5a731f7dbe8c05429c96217eb4f4a2ee31e15831
Salim <doshka23@gmail.com>
2022-01-24 14:58:12 +0100

    ProfileTop & ProfileAbout Components

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -3,11 +3,13 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect} from 'react-redux';
 import Spinner from '../layout/Spinner';
+import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
 import { getProfileById} from '../../actions/profileAction';
 const Profile = ({getProfileById, profile: {profile, loading}, match , auth}) => {
     useEffect(( ) => {
         getProfileById(match.params.id);
-    }, [ getProfileById]);
+    }, [ getProfileById, match.params.id]);
   return <Fragment>
       {profile === null || loading ? <Spinner/> : <Fragment>
           <Link to='/profiles' className='btn btn-light'>
@@ -16,6 +18,10 @@ const Profile = ({getProfileById, profile: {profile, loading}, match , auth}) =>
           {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (<Link to='/edit-profile' className='btn btn-dark'>
                 Edit Profile
           </Link>) }
+          <div className="profile-gride my-1">
+              <ProfileTop profile={profile}/>
+              <ProfileAbout profile={profile}/>
+          </div>
       </Fragment> }
   </Fragment>
 };
