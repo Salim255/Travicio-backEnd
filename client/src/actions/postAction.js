18dcb6eec5326b54e0b65edca661b522aa6709f8
Salim <doshka23@gmail.com>
2022-01-25 23:12:07 +0100

    Like & Unlike Functionality

--- client/src/actions/postAction.js
+++ client/src/actions/postAction.js
@@ -1,18 +1,58 @@
 import axios from 'axios';
 import { setAlert} from './alertAction';
-import { GET_POSTS, POST_ERROR } from './actionTypes';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES, USER_LOADED } from './actionTypes';
 
 //Get posts
 export const getPosts = () => async dispatch =>{
     try {
-        console.log("🌍🌔");
+        
         const res = await axios.get('/api/posts');
-        console.log("🌍🌔", res);
+       
         dispatch({
             type: GET_POSTS,
             payload: res.data
         })
     } catch (error) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: error.response.statusText, status: error.response.status}
+        })
+    }
+};
+//ADD like to posts
+export const addLike = (id) => async dispatch =>{
+    try {
+        
+        const res = await axios.put(`/api/posts/like/${id}`);
+        
+        dispatch({
+            type: UPDATE_LIKES,
+            payload: { id, likes: res.data}
+        })
+        
+    } catch (error) {
+        
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: error.response.statusText, status: error.response.status}
+        })
+    }
+};
+
+//Remove like to posts
+export const removeLike = (id) => async dispatch =>{
+    try {
+        
+        const res = await axios.put(`/api/posts/unlike/${id}`);
+       console.log('🌎',res);
+        dispatch({
+            type: UPDATE_LIKES,
+            payload: { id, likes:res.data}
+        })
+
+       
+    } catch (error) {
+       
         dispatch({
             type: POST_ERROR,
             payload: { msg: error.response.statusText, status: error.response.status}
--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { connect} from 'react-redux';
-
-const PostItem = ({auth, post: {_id, text, name, avatar, user, likes, comments, date}}) =><div class="post bg-white p-1 my-1">
+import { addLike, removeLike } from '../../actions/postAction';
+const PostItem = ({addLike, removeLike, auth, post: {_id, text, name, avatar, user, likes, comments, date}}) =><div class="post bg-white p-1 my-1">
   <div>
     <a href="profile.html">
       <img
@@ -22,11 +22,11 @@ const PostItem = ({auth, post: {_id, text, name, avatar, user, likes, comments, 
      <p class="post-date">
         Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
     </p>
-    <button type="button" class="btn btn-light">
+    <button  onClick={e => addLike(_id)} type="button" class="btn btn-light">
       <i class="fas fa-thumbs-up"></i>{' '}
       <span>{likes.length > 0 && (<span >{likes.length}</span>)}</span>
     </button>
-    <button type="button" class="btn btn-light">
+    <button onClick={e => removeLike(_id)} type="button" class="btn btn-light">
       <i class="fas fa-thumbs-down"></i>
     </button>
     <Link to={`/post/${_id}`} class="btn btn-primary">
@@ -53,4 +53,4 @@ const mapStateToProps = state =>({
     
 });
 
-export default connect(mapStateToProps, {})(PostItem);
+export default connect(mapStateToProps, {addLike, removeLike})(PostItem);
